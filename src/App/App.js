07dcf9fc45b5788e8b760d037f1a8e832ad64b38
champix56 <desorbaix@free.fr>
2021-11-17 14:57:46 +0100

    reformat

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import './App.css';
-import Button from './components/Button/Button'
+import React from "react";
+import "./App.css";
+import Button from "./components/Button/Button";
 function App() {
   return (
     <div className="App">
-      <Button text="OK" onButtonClicked={(arg)=>{
-        alert('click sur button')
-      }}/>
-      <Button text="cancel"/>
-      <Button text="User ne clique pas ICI"  onButtonClicked={(arg)=>{
-        console.log(arg)
-        alert('Le user a osé cliquer, il est vilain le user')
-      }}/>
-      <Button text="Je sais que tu vas quand meme cliquer"/>
+      <Button
+        text="OK"
+        onButtonClicked={(arg) => {
+          alert("click sur button");
+        }}
+      />
+      <Button text="cancel" />
+      <Button
+        text="User ne clique pas ICI"
+        onButtonClicked={(arg) => {
+          console.log(arg);
+          alert("Le user a osé cliquer, il est vilain le user");
+        }}
+      />
+      <Button text="Je sais que tu vas quand meme cliquer" />
     </div>
   );
 }
