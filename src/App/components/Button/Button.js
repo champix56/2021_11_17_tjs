69e49a367c2997fc2956d025f984456f43b4136d
champix56 <desorbaix@free.fr>
2021-11-17 14:13:03 +0100

    creation de composant simple

---
+++ src/App/components/Button/Button.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Button.css";
+/**
+ * Composant de button html
+ * @param {object} props props du composant
+ * @returns component render
+ */
+const Button = (props) => {
+  return <button className="Button">Benjamin button</button>;
+};
