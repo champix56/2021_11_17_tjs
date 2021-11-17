0e938373f9e0c4837a2de0192fa0d7c122c002be
champix56 <desorbaix@free.fr>
2021-11-17 16:37:07 +0100

    lifcycle class didupdate

--- src/App/App.js
+++ src/App/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import Button from './components/Button/Button'
+import React from "react";
+import Button from "./components/Button/Button";
 
-class App extends React.Component{
-  counter=0;
-  constructor(props)
-  {
+class App extends React.Component {
+  counter = 0;
+  constructor(props) {
     super(props);
+    this.state = { counter: 0, value2: 0 };
   }
-  render(){
-    return <div className="App">
-          counter:{this.counter}<br/>
-          <Button text="soustraire 1" onButtonClicked={()=>{
-            this.counter--; 
-            console.log(this.counter);
-          }} bgColor="tomato"/>
-          <Button text="Ajouter 1" onButtonClicked={()=>{
-            this.counter++; 
-            console.log(this.counter);
-          }} bgColor="green"/>
-    </div>
+  componentDidUpdate(){
+    console.log('%c%s','font-size:42pt;color:red;background-color:skyblue;border:1px solid black;text-align:center','le changement est pret et effectif'+this.state.counter)
+  }
+  render() {
+    return (
+      <div className="App">
+        counter:{this.state.counter}
+        <br />
+        <Button
+          text="soustraire 1"
+          onButtonClicked={() => {
+            this.setState({ counter: this.state.counter - 1 });
+            console.log(this.state);
+          }}
+          bgColor="tomato"
+        />
+        <Button
+          text="Ajouter 1"
+          onButtonClicked={() => {
+            this.setState({ counter: this.state.counter + 1 });
+            console.log(this.state);
+          }}
+          bgColor="green"
+        />
+      </div>
+    );
   }
 }
 export default App;
