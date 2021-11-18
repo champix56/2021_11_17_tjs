31720acc8ee562b6e7974136dad01093255a972b
champix56 <desorbaix@free.fr>
2021-11-18 12:25:23 +0100

    meo avec fragment

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Button from "./components/Button/Button";
+import FlexLayout from "./components/FlexLayout/FlexLayout";
+import Header from "./components/Header/Header";
+import MemeForm from "./components/MemeForm/MemeForm";
 import MemeViewer from "./components/MemeViewer/MemeViewer";
 
 class App extends React.Component {
@@ -17,30 +20,36 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div className="App">
-        <MemeViewer
-          meme={{
-            titre: "mon 1er meme",
-            text: "stop la triche",
-            x: 370,
-            y: 530,
-            fontSize: 27,
-            color: "tomato",
-            fontWeight: "900",
-            underline: true,
-            italic: true,
-            frameX: 0,
-            frameY: 0,
-          }}
-          image={{
-            id: 0,
-            url: "img/meme1.jpg",
-            titre: "meme1",
-            h: 778,
-            w: 736,
-          }}
-        />
-      </div>
+      <>
+        <Header />
+        <div className="App">
+          <FlexLayout>
+            <MemeViewer
+              meme={{
+                titre: "mon 1er meme",
+                text: "stop la triche",
+                x: 370,
+                y: 530,
+                fontSize: 27,
+                color: "tomato",
+                fontWeight: "900",
+                underline: true,
+                italic: true,
+                frameX: 0,
+                frameY: 0,
+              }}
+              image={{
+                id: 0,
+                url: "img/meme1.jpg",
+                titre: "meme1",
+                h: 778,
+                w: 736,
+              }}
+            />
+            <MemeForm />
+          </FlexLayout>
+        </div>
+      </>
     );
   }
 }
