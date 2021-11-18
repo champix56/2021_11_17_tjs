13e1effd6f5f8661eb64565ba9a509dfd23fba9e
champix56 <desorbaix@free.fr>
2021-11-18 11:49:56 +0100

    passage en ts du cmp avec protection image undefined

--- src/App/components/MemeViewer/MemeViewer.tsx
+++ src/App/components/MemeViewer/MemeViewer.tsx
@@ -1,30 +1,44 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./MemeViewer.module.scss";
+import { I_Image, I_Meme } from "../../interfaces/interfaces";
 
-const MemeViewer = (props) => (
-  <svg
-    className={styles.MemeViewer}
-    data-testid="MemeViewer"
-    viewBox={`0 0 ${props.image.w} ${props.image.h}`}
-  >
-    <image href={props.image.url} x="0" y="0" />
-    <text
-      x={props.meme.x}
-      y={props.meme.y}
-      fill={props.meme.color}
-      fontSize={props.meme.fontSize} 
-      textDecoration={props.meme.underline?'underline':'none'}
-      fontStyle={props.meme.italic?'italic':'normal'}
-      fontWeight={props.meme.fontWeight}
-      >{props.meme.text}</text>
-  </svg>
-);
+interface I_Props {
+  meme: I_Meme;
+  image?: I_Image;
+}
 
-MemeViewer.propTypes = {
-  meme: PropTypes.object.isRequired,
-  image: PropTypes.object,
+const MemeViewer: React.FC<I_Props> = (props) => {
+  console.log(props);
+
+  return (
+    <svg
+      className={styles.MemeViewer}
+      data-testid="MemeViewer"
+      viewBox={`0 0 ${props.image ? props.image.w : 1000} ${
+        props.image ? props.image.h : 1000
+      }`}
+    >
+      {props.image && <image href={props.image.url} x="0" y="0" />}
+      <text
+        x={props.meme.x}
+        y={props.meme.y}
+        fill={props.meme.color}
+        fontSize={props.meme.fontSize}
+        textDecoration={props.meme.underline ? "underline" : "none"}
+        fontStyle={props.meme.italic ? "italic" : "normal"}
+        fontWeight={props.meme.fontWeight}
+      >
+        {props.meme.text}
+      </text>
+    </svg>
+  );
 };
+/* remplace par le type en ts*/
+// MemeViewer.propTypes = {
+//   meme: PropTypes.object.isRequired,
+//   image: PropTypes.object,
+// };
 
 MemeViewer.defaultProps = {};
 
