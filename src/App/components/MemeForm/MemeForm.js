329837b28d21c1db40fae681728d7c07ce3000f7
champix56 <desorbaix@free.fr>
2021-11-18 16:29:08 +0100

    mise en place du form avec binding

--- src/App/components/MemeForm/MemeForm.js
+++ src/App/components/MemeForm/MemeForm.js
@@ -14,22 +14,55 @@ function MemeForm(props) {
   return <div data-testid="MemeForm" className={styles.MemeForm}>
     <form>
       <h1>Titre</h1>
-      <input type="text" id="f_titre" placeholder="saisir titre" />
+      <input type="text" id="f_titre" placeholder="saisir titre" value={props.meme.titre} onChange={(evt) => {
+        props.onMemeChange({ ...props.meme, titre: evt.target.value })
+      }}  />
       <hr />
       <h2>Image</h2>
-      <select  >
+      <select value={props.meme.imageId} onChange={(evt) => {
+        props.onMemeChange({ ...props.meme, imageId: Number(evt.target.value) })
+      }}  >
         <option value="-1">Aucune</option>
-        {props.images.map((e,i)=><option value={e.id} >{e.titre}</option>)}
+        {props.images.map((e, i) => <option value={e.id} >{e.titre}</option>)}
       </select>
       <hr />
       <h2>text</h2>
-      <input type="text" />
+      <input type="text" value={props.meme.text} onChange={(evt) => {
+        props.onMemeChange({ ...props.meme, text: evt.target.value })
+      }} />
       <div className={styles.half}>
-        <div><label htmlFor="f_x">x:</label><br /><input type="number" className={styles.smallInput} /></div>
-        <div><label htmlFor="f_y">y:</label><br /><input type="number" className={styles.smallInput} /></div>
+        <div>
+          <label htmlFor="f_x">x:</label><br />
+          <input type="number" className={styles.smallInput} value={props.meme.x}  onChange={(evt)=>{
+        props.onMemeChange({...props.meme, x: Number(evt.target.value)})
+      }}  />
+        </div>
+        <div>
+          <label htmlFor="f_y">y:</label><br />
+          <input type="number" className={styles.smallInput} value={props.meme.y}   onChange={(evt)=>{
+        props.onMemeChange({...props.meme, y: Number(evt.target.value)})
+      }}  />
+        </div>
       </div>
-
       <hr />
+      <label htmlFor="f_color">Couleur</label>
+      <input type="color" id="f_color" value={props.meme.color}  onChange={(evt) => {
+        props.onMemeChange({ ...props.meme, color: evt.target.value })
+      }} />
+      <div className={styles.half}>
+        <div>
+        <label htmlFor="f_underline">underline:</label><br />
+        <input id="f_underline" type="checkbox" checked={props.meme.underline}  onChange={(evt) => {
+        props.onMemeChange({ ...props.meme, underline: evt.target.checked })
+      }}  />
+        </div>
+        <div>
+        <label htmlFor="f_italic">italic:</label><br />
+        <input id="f_italic" type="checkbox" checked={props.meme.italic} onChange={(evt) => {
+        props.onMemeChange({ ...props.meme, italic: evt.target.checked })
+      }}  />
+        </div>
+      </div>
       <div className={styles.half}>
         <Button type="reset" bgColor="tomato" text="clear" />
         <Button type="submit" bgColor="skyblue" text="save" />
@@ -41,6 +74,6 @@ function MemeForm(props) {
 MemeForm.propTypes = {
   meme: PropTypes.object.isRequired,
   images: PropTypes.array.isRequired,
-  onMemeChange : PropTypes.func.isRequired,
+  onMemeChange: PropTypes.func.isRequired,
 };
 export default MemeForm;
