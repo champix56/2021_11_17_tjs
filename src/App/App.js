969d3ef7b6b83a9585a2b877f3eddeb6a9f7fb74
champix56 <desorbaix@free.fr>
2021-11-19 10:58:59 +0100

    connexion au rest d'un cmp

--- src/App/App.js
+++ src/App/App.js
@@ -4,7 +4,8 @@ import FlexLayout from "./components/FlexLayout/FlexLayout";
 import Header from "./components/Header/Header";
 import MemeForm from "./components/MemeForm/MemeForm";
 import MemeViewer from "./components/MemeViewer/MemeViewer";
-
+import { REST_ADR, REST_RESSOURCES } from "./config/config";
+import store from './store/store'
 class App extends React.Component {
   counter = 0;
   constructor(props) {
@@ -21,29 +22,19 @@ class App extends React.Component {
         frameX: 0, frameY: 0,
         imageId: 0
       },
-      images: [
-        {
-          id: 0,
-          url: "img/meme1.jpg",
-          titre: "meme1",
-          h: 778,
-          w: 736,
-        },
-        {
-          id: 1,
-          url: "img/camera.png",
-          titre: "camera",
-          h: 768,
-          w: 922,
-        }
-      ]
+      images: [ ]
     };
+  }
+  componentDidMount(){
+    fetch(`${REST_ADR}${REST_RESSOURCES.images}`)
+      .then(f=>f.json())
+      .then(arr=>this.setState({images:arr}))
   }
   componentDidUpdate() {
     console.log(
       "%c%s",
       "font-size:42pt;color:red;background-color:skyblue;border:1px solid black;text-align:center",
-      "le changement est pret et effectif" + this.state.counter
+      "le changement est pret et effectif" + JSON.stringify(this.state)
     );
   }
   render() {
