8a1aec3ee956119c86137038064cdc9d2c77471b
champix56 <desorbaix@free.fr>
2021-11-19 17:21:31 +0100

    fin semaine

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,12 @@ import React from "react";
 import { connect } from "react-redux";
 import Header from "./components/Header/Header";
 import MemeThumbnail from "./components/MemeThumbnail/MemeThumbnail";
+import { Route, Switch } from 'react-router-dom'
+import FlexLayout from "./components/FlexLayout/FlexLayout";
+import MemeViewer from "./components/MemeViewer/MemeViewer";
+import MemeForm from "./components/MemeForm/MemeForm";
+import Navbar from "./components/Navbar/Navbar";
+
 class App extends React.Component {
   componentDidUpdate() {
     console.log(
@@ -15,35 +21,44 @@ class App extends React.Component {
     return (
       <>
         <Header />
+        <Navbar/>
         <div className="App">
-          <MemeThumbnail/>
-          {/* <FlexLayout>
-            <MemeViewer
-              meme={this.props.current}
-              image={this.props.images.find((e) => e.id === this.props.current.imageId)}
-            />
-           <MemeForm
-             
-              // onMemeChange={(meme) => {
-              //   this.setState({ current: meme })
-              // }}
-           
-            />  
-          </FlexLayout> */}
+          <Switch>
+            <Route path="/" exact>
+              <h1>Bonjour et bienvenue</h1>
+              voici le nouveau generateur de meme version 2021
+            </Route>
+            <Route path="/thumbnail">
+              <MemeThumbnail />
+            </Route>
+            <Route path="/edit">
+              <FlexLayout>
+                <MemeViewer
+                  meme={this.props.current}
+                  image={this.props.images.find((e) => e.id === this.props.current.imageId)}
+                />
+                <MemeForm/>
+              </FlexLayout>
+            </Route>
+            <Route path="/" >
+            <h1>404 !! NOT FOUND</h1>
+            </Route>
+          </Switch>
+
+
         </div>
       </>
     );
   }
 }
-function mapStateToProps(state,own){
+function mapStateToProps(state, own) {
   return {
     ...own,
-    current:state.current,
-    images:state.ressources.images
+    current: state.current,
+    images: state.ressources.images
   }
 }
-function mapDispatchToProps(dispatch)
-{
+function mapDispatchToProps(dispatch) {
   return {}
 }
-export default connect(mapStateToProps,mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
