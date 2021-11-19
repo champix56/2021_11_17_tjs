edf1af073a5610d14a1c4858448d473339a5636d
champix56 <desorbaix@free.fr>
2021-11-19 11:57:06 +0100

    store ok avec combine

--- src/App/store/store.js
+++ src/App/store/store.js
@@ -1,4 +1,5 @@
-import {createStore} from 'redux';
+import { combineReducers, createStore } from 'redux';
+import { REST_ADR, REST_RESSOURCES } from '../config/config';
 export const ressourcesInitialState = {
     memes: [],
     images: []
@@ -9,8 +10,17 @@ export const RESSOURCES_PUBLIC_ACTIONS = Object.freeze({
     ADD_MEME: 'ADD_MEME',
 })
 function ressourceReducer(state = ressourcesInitialState, action) {
-    console.log(action);
     switch (action.type) {
+        case 'INIT':
+            const pimage = fetch(`${REST_ADR}${REST_RESSOURCES.images}`).then(f => f.json())
+            const pmemes = fetch(`${REST_ADR}${REST_RESSOURCES.memes}`).then(f => f.json())
+            Promise.all([pimage, pmemes])
+                .then(arr_arr => {
+                    store.dispatch({ type: 'INIT_ARRAYS', values: arr_arr })
+                })
+            return state;
+        case 'INIT_ARRAYS':
+            return { ...state, images: action.values[0], memes: action.values[1] };
         case RESSOURCES_PUBLIC_ACTIONS.REPLACE_IMAGES_LIST:
             return { ...state, images: action.values };
         case RESSOURCES_PUBLIC_ACTIONS.REPLACE_MEMES_LIST:
@@ -21,13 +31,47 @@ function ressourceReducer(state = ressourcesInitialState, action) {
     }
 }
 
-const store=createStore(ressourceReducer);
-store.subscribe(()=>{
+
+export const currentInitialState = {
+    titre: "",
+    text: "",
+    x: 0, y: 10,
+    fontSize: 10,
+    color: "#000000",
+    fontWeight: "500",
+    underline: false, italic: false,
+    frameX: 0, frameY: 0,
+    imageId: -1
+}
+const CURRENT_PUBLIC_ACTIONS = Object.freeze({
+    UPDATE_CURRENT: 'UPDATE_CURRENT',
+    CLEAR_MEME: 'CLEAR_MEME',
+    SAVE_MEME: 'SAVE_MEME'
+})
+const currentReducer = (state = currentInitialState, action) => {
+    console.log(action.type);
+    switch (action.type) {
+        case 'INIT':
+            console.log('Current a recu INIT');
+            return state;
+        case CURRENT_PUBLIC_ACTIONS.UPDATE_CURRENT:
+            return { ...action.value }
+        case CURRENT_PUBLIC_ACTIONS.CLEAR_MEME:
+            return { ...currentInitialState }
+        default:
+            return state
+    }
+}
+
+
+const store = createStore(combineReducers({ current: currentReducer, ressources: ressourceReducer }));
+store.subscribe(() => {
     console.log(store.getState());
 });
-store.dispatch({ type:RESSOURCES_PUBLIC_ACTIONS.REPLACE_IMAGES_LIST, values: [{ id: 0 }, { id: 1 }] })
-store.dispatch({ type:RESSOURCES_PUBLIC_ACTIONS.REPLACE_MEMES_LIST, values: [{ id: 10 }, { id: 11 }] })
-store.dispatch({ type:RESSOURCES_PUBLIC_ACTIONS.ADD_MEME, value: { id: 20 }})
+store.dispatch({ type: 'INIT' })
+// store.dispatch({ type: RESSOURCES_PUBLIC_ACTIONS.REPLACE_IMAGES_LIST, values: [{ id: 0 }, { id: 1 }] })
+// store.dispatch({ type: RESSOURCES_PUBLIC_ACTIONS.REPLACE_MEMES_LIST, values: [{ id: 10 }, { id: 11 }] })
+// store.dispatch({ type: RESSOURCES_PUBLIC_ACTIONS.ADD_MEME, value: { id: 20 } })
 
 
 export default store;
