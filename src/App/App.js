848c83c3f880abf3ba2a516a3b8124cf4082cbf8
champix56 <desorbaix@free.fr>
2021-11-19 16:27:24 +0100

    mise en place d'un thumbnail

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
-import Button from "./components/Button/Button";
-import FlexLayout from "./components/FlexLayout/FlexLayout";
 import Header from "./components/Header/Header";
-import MemeForm from "./components/MemeForm/MemeForm";
-import MemeViewer from "./components/MemeViewer/MemeViewer";
-import { REST_ADR, REST_RESSOURCES } from "./config/config";
-import store, { currentInitialState, ressourcesInitialState } from './store/store'
+import MemeThumbnail from "./components/MemeThumbnail/MemeThumbnail";
 class App extends React.Component {
   componentDidUpdate() {
     console.log(
       "%c%s",
       "font-size:10pt;color:red;background-color:skyblue;border:1px solid black;text-align:center",
-      "le changement est pret et effectif" + JSON.stringify(this.state)
+      "le changement est pret et effectif" + JSON.stringify(this.props)
     );
   }
   render() {
@@ -21,7 +16,8 @@ class App extends React.Component {
       <>
         <Header />
         <div className="App">
-          <FlexLayout>
+          <MemeThumbnail/>
+          {/* <FlexLayout>
             <MemeViewer
               meme={this.props.current}
               image={this.props.images.find((e) => e.id === this.props.current.imageId)}
@@ -33,7 +29,7 @@ class App extends React.Component {
               // }}
            
             />  
-          </FlexLayout>
+          </FlexLayout> */}
         </div>
       </>
     );
